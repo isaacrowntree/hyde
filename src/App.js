68bee3b70852d884d303c17635f39b0251d78648
Isaac Rowntree <isaac@rowntree.me>
2017-06-01 22:29:35 +1000

    Initial discovery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Files from './components/Files/Files.js';
 
+// Login to appear in modal window
+// Need bootstrap JS in react setup 
+// https://github.com/react-bootstrap/react-bootstrap
+// Show the login over page like with seedr.cc
+// Lock the page otherwise (by simply not loading state or data)
+
 class App extends Component {
   render() {
     return (
