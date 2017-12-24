6fb014e6c398d620ffb677e05bf1154d13b69c4d
Isaac Rowntree <isaac.rowntree@redbubble.com>
2017-12-24 18:23:07 +1100

    Renamed routes back down

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './Routes';
+import Routes from './routes';
 import { Provider } from 'react-redux';
 import { Store } from './Redux';
 import './index.css';
