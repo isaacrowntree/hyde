715859fa582e3b9bc9267a665cd0354a10eb9bed
Isaac Rowntree <isaac.rowntree@redbubble.com>
2017-12-25 01:15:26 +1100

    Run dev by default

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ var config = {};
 
 if (process.env.NODE_ENV === 'test') {
   config = require('./config/test.js');
-} else if (process.env.NODE_ENV === 'development') {
-  config = require('./config/development.js');
-} else {
+} else if (process.env.NODE_ENV === 'production') {
   config = require('./config/production.js');
+} else {
+  config = require('./config/development.js');
 }
 
 module.exports = config;
