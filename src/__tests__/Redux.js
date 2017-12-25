00773853d446d4956a3c64d8622ac99148ebad3c
Isaac Rowntree <isaac.rowntree@redbubble.com>
2017-12-25 14:22:23 +1100

    Removed unnecessary test

--- src/__tests__/Redux.js
+++
@@ -1,7 +0,0 @@
-import { comparePassword } from '../Redux';
-import CryptoJS from 'crypto-js';
-
-test('Can decrypt a password', () => {
-  expect(comparePassword('test')).toEqual(true);
-  expect(comparePassword('not_a_password')).toEqual(false);
-});
